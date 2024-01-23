88902c5dbed4df594f7ab5c74bc01d4507112f39
Andrew Romitti <132237865+AndrewRomitti@users.noreply.github.com>
2024-01-22 22:25:48 -0600

    Add files via upload

---
+++ move_real_data.py
@@ -0,0 +1,13 @@
+import shutil
+import os
+
+PATH = "/mydata/Data/"
+DEST = "/mydata/Synthetic_25"
+
+DEST_SUB_DIRS = os.listdir(DEST)
+
+for subdir in os.listdir(PATH):
+	for file in os.listdir(PATH/subdir)[:(len(os.listdir(PATH/subdir))*0.75)]:
+		current_dir = DEST_SUB_DIRS[os.listdir(PATH).index(subdir)]
+		shutil.move(PATH/subdir/file, DEST/DEST_SUB_DIRS)
+		
