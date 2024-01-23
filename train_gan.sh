b9c66767408b7f8ce329b6dce9f93517e0369d4b
Andrew Romitti <132237865+AndrewRomitti@users.noreply.github.com>
2024-01-23 00:08:32 -0600

    Update train_gan.sh

--- train_gan.sh
+++ train_gan.sh
@@ -10,6 +10,7 @@ else
         exit 1
 fi
 
+#Installing Kaggle
 cp kaggle.json ~/.kaggle/
 
 chmod 600 ~/.kaggle/kaggle.json
@@ -22,15 +23,20 @@ kaggle datasets download -d ninadaithal/imagesoasis
 
 unzip /mydata/imagesoasis.zip
 
+#Running python script to prepare for conditional training
 python3 dataset_preperation.py
 
+#creating stylegan3 repo
 git clone https://github.com/NVlabs/stylegan3.git
 
 mkdir final_dataset
 mkdir train_logs
 
 cd stylegan3
+#Creating stylegan-ready dataset
 python3 dataset_tool.py --source=/mydata/Data/ --dest=/mydata/final_dataset/oasisdataset_256x256.zip --resolution=256x256 --cond=1
+
+#Training stylegan, hyperparameters taken from stylegan github repo
 python3 train.py --outdir=/mydata/train_logs --cfg=stylegan2 --data=/mydata/final_dataset/oasisdataset_256x256.zip --gpus=1 --batch=16 --gamma=6.6 --mir>
 
 echo "Training finished"
