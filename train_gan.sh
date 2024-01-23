5b75dcbd917fb4b840e63d6a876275bb2f8e1fca
Andrew Romitti <132237865+AndrewRomitti@users.noreply.github.com>
2024-01-22 21:50:21 -0600

    Create train_gan.sh

---
+++ train_gan.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Creating kaggle directory
+mkdir -p ~/.kaggle
+
+if [ -f $kaggle.json]; then
+        echo "kaggle.json found moving to directory"
+else
+        echo "kaggle.json does not exist, you must create a kaggle.json file in the home directory"
+        exit 1
+fi
+
+cp kaggle.json ~/.kaggle/
+
+chmod 600 ~/.kaggle/kaggle.json
+
+pip install kaggle
+
+cd /mydata
+
+kaggle datasets download -d ninadaithal/imagesoasis
+
+unzip /mydata/imagesoasis.zip
+
+python3 dataset_preperation.py
+
+git clone https://github.com/NVlabs/stylegan3.git
+
+mkdir final_dataset
+mkdir train_logs
+
+cd stylegan3
+python3 dataset_tool.py --source=/mydata/Data/ --dest=/mydata/final_dataset/oasisdataset_256x256.zip --resolution=256x256 --cond=1
+python3 train.py --outdir=/mydata/train_logs --cfg=stylegan2 --data=/mydata/final_dataset/oasisdataset_256x256.zip --gpus=1 --batch=16 --gamma=6.6 --mir>
+
+echo "Training finished"
