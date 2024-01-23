0b6ad37fc336d22b56e3c6bc89a69ebdd2d6227f
Andrew Romitti <132237865+AndrewRomitti@users.noreply.github.com>
2024-01-23 00:09:22 -0600

    Update split_dataset.sh

--- split_dataset.sh
+++ split_dataset.sh
@@ -7,6 +7,8 @@ echo "What dataset split would you like to create? Please type: 25%"
 read $decision
 
 if[$decision=="25%"]; then
+
+        #Creating synthetic directory that mimicks real directories
         mkdir Synthetic_25
         cd Synthetic_25
         mkdir Mild_Dementia
@@ -17,6 +19,7 @@ if[$decision=="25%"]; then
         cd --
         cd /mydata
 
+        #generating images per each class, images mimick 25% of real dataset composition per class
         cd stylegan3
         python3 gen_images.py --outdir=/mydata/Synthetic_25/Mild_Dementia --trunc=1 --seeds=0-1250 --network=/mydata/train_logs/training-runs/05000-stylegan2-imagesoasis256x256-gpus1-batch16-gamma6.6/network-snapshot-5000.pkl --class=0
         python3 gen_images.py --outdir=/mydata/Synthetic_25/Moderate_Dementia --trunc=1 --seeds=0-122 --network=/mydata/train_logs/training-runs/05000-stylegan2-imagesoasis256x256-gpus1-batch16-gamma6.6/network-snapshot-5000.pkl --class=1
