066e1c723a55bdfb00c61d955313dbc96c51b15f
Andrew Romitti <132237865+AndrewRomitti@users.noreply.github.com>
2024-01-22 22:00:07 -0600

    Add files via upload

---
+++ dataset_preperation.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+dummy_son = {
+	"labels":
+		[
+
+		]
+}
+
+for subdir in os.listdir(/mydata/Data):
+	for file in os.listdir(/mydata/Data/subdir):
+		dummy_son["labels"].append([subdir/file, os.listdir(/mydata/Data/).index(subdir)])
+
+json_object = json.dumps(dummy_son)
+
+with open("/mydata/Data/dataset.json", "w") as outfile:
+	outfile.write(json_object)
