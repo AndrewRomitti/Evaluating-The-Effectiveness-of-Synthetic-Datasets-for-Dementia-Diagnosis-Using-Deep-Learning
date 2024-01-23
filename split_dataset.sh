0448abf94c2f169657cb6e279ebb21330e0a4991
Andrew Romitti <132237865+AndrewRomitti@users.noreply.github.com>
2024-01-22 22:27:35 -0600

    Create split_dataset.sh

---
+++ split_dataset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /mydata
+
+echo "What dataset split would you like to create? Please type: 25%"
+read $decision
+
+if[$decision=="25%"]; then
+        mkdir Synthetic_25
+        cd Synthetic_25
+        mkdir Mild_Dementia
+        mkdir Moderate_Dementia
+        mkdir Non_Demented
+        mkdir Very_mild_Dementia
+
+        cd --
+        cd /mydata
+
+        cd stylegan3
+        python3 gen_images.py --outdir=/mydata/Synthetic_25/Mild_Dementia --trunc=1 --seeds=0-1250 --network=/mydata/train_logs/training-runs/05000-stylegan2-imagesoasis256x256-gpus1-batch16-gamma6.6/network-snapshot-5000.pkl --class=0
+        python3 gen_images.py --outdir=/mydata/Synthetic_25/Moderate_Dementia --trunc=1 --seeds=0-122 --network=/mydata/train_logs/training-runs/05000-stylegan2-imagesoasis256x256-gpus1-batch16-gamma6.6/network-snapshot-5000.pkl --class=1
+        python3 gen_images.py --outdir=/mydata/Synthetic_25/Non_Demented --trunc=1 --seeds=0-16800 --network=/mydata/train_logs/training-runs/05000-stylegan2-imagesoasis256x256-gpus1-batch16-gamma6.6/network-snapshot-5000.pkl --class=2
+        python3 gen_images.py --outdir=/mydata/Synthetic_25/Very_mild_Dementia --trunc=1 --seeds=0-3425 --network=/mydata/train_logs/training-runs/05000-stylegan2-imagesoasis256x256-gpus1-batch16-gamma6.6/network-snapshot-5000.pkl --class=3
+
